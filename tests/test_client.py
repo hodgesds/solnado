762826a37b342b7a817301dbffbe0d549729c683
Daniel Hodges <hodges.daniel.scott@gmail.com>
2015-11-02 20:53:03 -0600

    update tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,15 +1,26 @@
-from tornado.testing import AsyncTestCase
+import json
+from functools import partial
+from nose.tools import ok_, eq_
 from solnado import SolrClient
+from tornado import gen
+from tornado.testing import AsyncTestCase, gen_test
 
 
 class ClientTestCase(AsyncTestCase):
+    def setUp(self):
+        super(ClientTestCase, self).setUp()
+        self.client = SolrClient()
+
     def test_mk_req(self):
-        c = SolrClient()
-        self.assertEquals(c.base_url, c.mk_req('').url)
-        self.assertEquals('GET', c.mk_req('').method)
+        self.assertEquals(self.client.base_url, self.client.mk_req('').url)
+        self.assertEquals('GET', self.client.mk_req('').method)
 
     def test_mk_url(self):
-        c = SolrClient()
-        url = c.mk_url(*['a','b','c'], **{'key':'value'})
+        url = self.client.mk_url(*['a','b','c'], **{'key':'value'})
         self.assertEquals('/a/b/c?key=value', url)
 
+    @gen_test(timeout=10)
+    def test_core_status(self):
+        res = yield gen.Task(partial(self.client.core_status))
+        ok_(json.loads(res.body))
+        eq_(200, res.code)
