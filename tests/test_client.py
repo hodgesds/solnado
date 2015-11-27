33400086cca6a8bc5195109903aabecaf9a7eb3c
Daniel Hodges <hodges.daniel.scott@gmail.com>
2015-11-27 08:05:53 -0600

    add schema field type test

--- tests/test_client.py
+++ tests/test_client.py
@@ -261,3 +261,13 @@ class ClientTestCase(AsyncTestCase):
         ))
         eq_(200, res.code)
         yield gen.Task(partial(self.client.delete_collection, 'bix'))
+
+    @gen_test(timeout=30)
+    def test_schema_field_types(self):
+        yield gen.Task(partial(self.client.delete_collection, 'bix'))
+        yield gen.Task(partial(self.client.create_collection, 'bix'))
+        res = yield gen.Task(partial(
+            self.client.schema_field_types, 'bix'
+        ))
+        eq_(200, res.code)
+        yield gen.Task(partial(self.client.delete_collection, 'bix'))
