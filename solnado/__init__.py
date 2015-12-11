43ab225870950c7733f0dda7a6e7d9aa95f1e855
Daniel Hodges <hodges.daniel.scott@gmail.com>
2015-12-10 19:57:16 -0600

    bump version

--- solnado/__init__.py
+++ solnado/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 from .client import SolrClient
-VERSION = (0, 9, 2)
+VERSION = (0, 9, 3)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
